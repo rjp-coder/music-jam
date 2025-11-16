a003261af18f251ace6189e756e74863df68cb55
Ryan Parry <rj-parry@msn.com>
2025-11-16 04:56:27 +0000

    update config with base url for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/music-jam",
   plugins: [react(), tailwindcss()],
   // test: { include: ["./src/**/*.test.tsx?"] },
 });
